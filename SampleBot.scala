afd11599dafc845c94845a36797a77cd07dbda79
vijay <vj@vj-laptop.local>
2011-05-30 21:05:11 -0700

    pull out connection to service and bot logic as traits

--- SampleBot.scala
+++ SampleBot.scala
@@ -8,10 +8,267 @@ import scala.util.parsing.json.JSON
 import scala.util.parsing.json.JSON._
 
 trait Logger {
-  val VERBOSE = false
+  val VERBOSE = true
 
   def yap(msg: => String) = if (VERBOSE) println(msg)
 }
+
+trait JsonHelpers {
+  def asMap(obj:Any):Option[Map[String, Any]] = {
+    obj match {
+      case m:Map[String, Any] => Some(m)
+      case Some(m:Map[String, Any]) => Some(m)
+      case _ => None
+    }
+  }
+  
+  def asList(obj:Any):Option[List[Any]] = {
+    obj match {
+      case a:List[Any] => Some(a)
+      case Some(a:List[Any]) => Some(a)
+      case _ => None
+    }
+  }
+  
+  def asDouble(obj:Any):Option[Double] = {
+    try {
+      obj match {
+        case d:Double => Some(d)
+        case Some(d:Double) => Some(d)
+        case Some(a:Any) => Some(a.toString.trim.toDouble)
+        case _ => Some(obj.toString.trim.toDouble)
+      }
+    } catch {
+      case _ => {
+        None
+      }
+    }
+  }
+
+  // poor man's xpath
+  def extract(map:Map[String, Any], path:String*):Option[Any] = {
+    var ret:Any = map
+    for (p <- path) {
+      val m = asMap(ret)
+      if (!m.isDefined) {
+        return None
+      }
+
+      m.get.get(p) match {
+        case Some(thing:Any) => ret = thing
+        case _ => return None
+      }
+    }
+
+    Some(ret)
+  }
+}
+
+trait OorbyPokerClient {
+  val pokerClient:OorbyPoker
+
+  trait OorbyPoker {
+    def take_action(action: String):Map[String, Any]
+    def get_next_event:Map[String, Any]
+  }
+}
+
+trait RemotePokerClient extends OorbyPokerClient {
+  val botName:String
+  val devKey:String
+
+  val GAME_CREATOR_HOST = "mcp.oorby.com"
+  var CURRENT_ENDPOINT_HOST = "http://" + GAME_CREATOR_HOST
+
+  val pokerClient = new OorbyPoker with JsonHelpers {
+    var last_results = ""
+    var last_event_id = "NONE"
+    val enc = "UTF-8"
+  
+    def take_action(action: String):Map[String, Any] = {
+      try {
+        val results = endpoint_post("/v1/poker/bots/" + botName + "/next_event", HashMap(Pair("action", action)))
+    
+        parseResults(results)
+      } catch {
+        case ex:HTTPException => if (ex.getStatusCode >= 400 && ex.getStatusCode < 500) {
+          return get_next_event
+        } else {
+          throw ex
+        }
+      }
+    }
+  
+    def get_next_event:Map[String, Any] = {
+      val results = endpoint_get("/v1/poker/bots/" + botName + "/next_event")
+
+      parseResults(results)
+    }
+
+
+    def http_get(url: String): String = {
+      // yap("")
+      // yap("GET('" + url + "')")
+      // yap("---")
+
+      makeReq(() => {
+        val u = new URL(url)
+        val conn = u.openConnection().asInstanceOf[HttpURLConnection]
+        conn.setReadTimeout(0) // no timeout
+        conn.setInstanceFollowRedirects(true)
+        readResponse(conn)
+      })
+    }
+  
+    def http_post(url: String, data: String): String = {
+      // yap("")
+      // yap("POST('" + url + "', '" + data + "')")
+      // yap("---")
+
+      makeReq(() => {
+        val u = new URL(url)
+        val conn = u.openConnection().asInstanceOf[HttpURLConnection]
+        conn.setDoOutput(true)
+        conn.setReadTimeout(0) // no timeout
+        conn.setInstanceFollowRedirects(true)
+
+        val wr = new OutputStreamWriter(conn.getOutputStream())
+        wr.write(data)
+        wr.flush
+        wr.close()
+
+        readResponse(conn)
+      })
+    }
+
+    private def makeReq(requestGen:() => (Int, String)):String = {
+      while (true) {
+        val (code, data) = requestGen()
+        if (code == 200) {
+          return data
+        } else if (code >= 500) {
+          // yap("got error from server, waiting 10 seconds before retrying " + code)
+          Thread.sleep(10)
+        } else if (code >= 400) {
+          throw new HTTPException(code)
+        } else {
+          // yap("got empty response from server " + code)
+        }
+      }
+
+      return ""
+    }
+
+    def readResponse(conn:HttpURLConnection):(Int, String) = {
+      // yap(conn.getResponseCode() + " " + conn.getResponseMessage())
+      val respCode = conn.getResponseCode()
+      val istr = if (conn.getResponseCode() < 300) { conn.getInputStream() } else { conn.getErrorStream() }
+      val in = new BufferedReader(new InputStreamReader(istr))
+      var line = new String
+      var stop = false
+    
+      var response = ""
+    
+      while (!stop) {
+        val line = in.readLine()
+        if (line == null)
+          stop = true
+        else {
+          response += line + "\n"
+          // yap(line)
+        }
+      }
+    
+      in.close()
+    
+      // yap("---")
+      // yap("")
+    
+      (respCode, response)
+    }
+
+    def endpoint_post(resource: String, parameters: HashMap[String, String]): String = {
+      val url = CURRENT_ENDPOINT_HOST + resource + "?devkey=" + devKey + "&eventId=" + last_event_id
+      val data = parameters.map(kv => URLEncoder.encode(kv._1, enc) + "=" + URLEncoder.encode(kv._2, enc)).mkString("&")
+
+      http_post(url, data)
+    }
+  
+    def endpoint_get(resource: String): String = {
+      val url = CURRENT_ENDPOINT_HOST + resource + "?devkey=" + devKey + "&eventId=" + last_event_id
+    
+      http_get(url)
+    }
+
+    def parseResults(results: String):Map[String, Any] = {
+      last_results = results
+    
+      for {
+        resultsJson <- JSON.parseFull(results)
+        resultsMap <- asMap(resultsJson)
+        event <- asMap(resultsMap.get("event"))
+        game <- asMap(event.get("game"))
+        eventId <- resultsMap.get("eventId")
+      } {
+        last_event_id = eventId.toString
+        return resultsMap      
+      }
+      Map()
+    }
+  }
+}
+
+
+trait Bot extends Logger with JsonHelpers {
+  self: OorbyPokerClient =>
+
+  def play = {
+    yap("Joining game, may take up to 30 seconds")
+
+    var continue = true
+    var nextAction:Option[String] = None
+    while (continue) {
+      val event = if (nextAction.isDefined) pokerClient.take_action(nextAction.get) else pokerClient.get_next_event
+
+      eventReceived(event)
+
+      val eventType = extract(event, "event", "eventType")
+      if (eventType.exists(_ == "ActionRequired")) {
+        nextAction = Some(decideNextAction(event))
+      }
+    }
+  }
+
+  def eventReceived(eventMap:Map[String, Any]) = {
+    println(eventMap)
+  }
+  def decideNextAction(eventMap:Map[String, Any]):String
+}
+
+
+class CallBot(val botName:String, val devKey:String) extends Bot
+                   with RemotePokerClient {
+  def decideNextAction(eventMap:Map[String, Any]):String = {
+    "c"
+  }
+}
+
+object BotRunner {
+  def main(args: Array[String]) {
+    if (args.size != 2) {
+      println("usage: scala BotRunner botName guid")
+      return
+    }
+
+    val bot = new CallBot(args(0), args(1))
+    bot.play
+  }
+}
+
+
+
+
+
 
 object SampleBot extends Logger
 {
